1b5ad863a67923cdfe98bd7ed433a3005b13097a
facu610 <Clio997>
2018-07-06 13:36:35 -0300

    FichaAct

---
+++ app/src/main/java/agronomia/coprotrab/agrokot/FichaActivity.kt
@@ -0,0 +1,12 @@
+package agronomia.coprotrab.agrokot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FichaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ficha)
+    }
+}
