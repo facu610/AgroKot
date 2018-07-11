f11bfe917b87697b6efe1a66d8ef8a6b0677ff31
facu610 <Clio997>
2018-07-11 13:41:11 -0300

    volley

--- app/src/main/java/agronomia/coprotrab/agrokot/MainActivity.kt
+++ app/src/main/java/agronomia/coprotrab/agrokot/MainActivity.kt
@@ -3,12 +3,17 @@ package agronomia.coprotrab.agrokot
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         val bCambiarInstr = findViewById<Button>(R.id.b_CambiarInstr)
         val bFichas = findViewById<Button>(R.id.b_Fichas)
+        val bSincro = findViewById<Button>(R.id.b_Sincro)
 
         tvInstr = findViewById<TextView>(R.id.tv_Instr)
         tvZona = findViewById<TextView>(R.id.tv_Zona)
@@ -44,13 +50,60 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         })
 
-
-
-
-        fun validaDato(et:EditText): Boolean {
-            if (et.text.isNullOrEmpty()) {return false}
-            return true
+        bSincro.setOnClickListener{
+            if (Network.validaConeccion(this)){
+                Toast.makeText(this, "Si hay red", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "No hay red", Toast.LENGTH_LONG).show()
+            }
+            if(Network.validaConeccion(this)){
+                Log.d("bSincroOnClick", descargarDatos( "https://google.com"))
+            }
+            else{
+                Toast.makeText(this,"No hay Internet", Toast.LENGTH_LONG).show()
+            }
         }
+    }
+    //Codigo Para Validar RED y Descargar Datos
+    private fun descargarDatos(url:String):String{
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        var inputStream: InputStream? = null
+
+        try {
+            val url = URL(url)
+            val conn = url.openConnection() as HttpsURLConnection
+            conn.requestMethod = "GET"
+            conn.connect()
+
+            inputStream = conn.inputStream
+            return inputStream.bufferedReader().use {
+                it.readText()
+            }
+        }
+        finally {
+            if(inputStream != null){
+                inputStream.close()
+            }
+        }
+    }
+
+
+    //Metodo Volley
+    private fun solicitudHTPPVolley(){
 
     }
+
+
+
+    fun validaDato(et:EditText): Boolean {
+        if (et.text.isNullOrEmpty()) {return false}
+        return true
+    }
 }
+
+
+
+
